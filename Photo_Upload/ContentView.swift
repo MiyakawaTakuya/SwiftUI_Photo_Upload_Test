4b0ad4c41dad1322093331b0f1b0e24ff9d3457a
Miyakawa Takuya <omyakawa@gmail.com>
2021-06-23 11:38:36 +0900

    Initial Commit

---
+++ Photo_Upload/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Photo_Upload
+//
+//  Created by 宮川卓也 on 2021/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Photo_Upload/Photo_UploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_UploadApp.swift
+//  Photo_Upload
+//
+//  Created by 宮川卓也 on 2021/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Photo_UploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
